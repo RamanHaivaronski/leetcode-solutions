b8a5a7f399d6f10f7cf23bcc55a2685703fd5427
RomanHaivaronski <romangaivoronsky@gmail.com>
2023-05-10 10:37:37 +0200

    14 Horizontal scanning

---
+++ CommonPrefix_14/Main.java
@@ -0,0 +1,11 @@
+package CommonPrefix_14;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] strs  = {"ab","a","asdsadsad"};
+        String result = s.longestCommonPrefix(strs);
+
+        System.out.println(result);
+    }
+}
---
+++ CommonPrefix_14/Solution.java
@@ -0,0 +1,42 @@
+package CommonPrefix_14;
+
+import java.util.Arrays;
+
+
+class Solution {
+    //Solution using sort
+    //compare smallest and largest words
+    // counter through smallestWOrd letters
+    //compare letters if true increment counter else break;
+    //return substring of smallestWord to counter
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while(c < first.length())
+        {
+            if (first.charAt(c) == last.charAt(c))
+                c++;
+            else
+                break;
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }
+
+
+    public String horizontalScanning(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+
+}
