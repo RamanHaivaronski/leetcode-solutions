3d288b487097064c60157ce81ad36277e554cce8
RomanHaivaronski <romangaivoronsky@gmail.com>
2023-05-12 10:25:27 +0200

    20 Stack matching parenthesis

---
+++ ValidParenthesis_20/Main.java
@@ -0,0 +1,10 @@
+package ValidParenthesis_20;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        boolean result = s.isValid1("()[]{()}");
+
+        System.out.println(result);
+    }
+}
---
+++ ValidParenthesis_20/Solution.java
@@ -0,0 +1,31 @@
+package ValidParenthesis_20;
+
+import java.util.Stack;
+
+public class Solution {
+
+    //stack FILO
+    // push - add to the top
+    // pop  - remove top elem
+    // peek - check top elem
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if(a == '(' || a == '[' || a == '{') {
+                stack.push(a);
+            } else if(stack.empty()) {
+                return false;
+            } else if(a == ')' && stack.pop() != '(') {
+                return false;
+            } else if(a == ']' && stack.pop() != '[') {
+                return false;
+            } else if(a == '}' && stack.pop() != '{') {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+
+
+}
