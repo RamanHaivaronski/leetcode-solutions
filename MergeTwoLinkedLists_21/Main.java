9caa228d95e0c3e1594aaf6995762b36d1ff8953
RomanHaivaronski <romangaivoronsky@gmail.com>
2023-05-15 08:57:12 +0200

    21 linked list

---
+++ MergeTwoLinkedLists_21/Main.java
@@ -0,0 +1,9 @@
+package MergeTwoLinkedLists_21;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        //ListNode result = s.mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(3))), new ListNode(2, new ListNode(5, new ListNode(6))));
+        ListNode result = s.mergeTwoLists(new ListNode(), new ListNode());
+    }
+}
--- ValidParenthesis_20/Main.java
+++ ValidParenthesis_20/Main.java
@@ -3,7 +3,7 @@ package ValidParenthesis_20;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        boolean result = s.isValid1("()[]{()}");
+        boolean result = s.isValid("()[]{()}");
 
         System.out.println(result);
     }
