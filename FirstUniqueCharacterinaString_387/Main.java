1cc3aa18d4643146479e8a8da87db5f37b0a50c8
RomanHaivaronski <romangaivoronsky@gmail.com>
2023-07-12 10:06:26 +0200

    242 387 string operations

---
+++ FirstUniqueCharacterinaString_387/Main.java
@@ -0,0 +1,10 @@
+package FirstUniqueCharacterinaString_387;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int result = s.firstUniqChar("aabb");
+
+        System.out.println(result);
+    }
+}
---
+++ IsAnagram_242/Solution.java
@@ -0,0 +1,20 @@
+package IsAnagram_242;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+    }
+}
