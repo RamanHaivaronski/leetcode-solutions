29f4b37d18bed18241f073147e73615780707ce5
RomanHaivaronski <romangaivoronsky@gmail.com>
2023-10-22 12:59:15 +0200

    350

---
+++ IntersectionofTwoArraysII_350/Main.java
@@ -0,0 +1,10 @@
+package IntersectionofTwoArraysII_350;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] result = s.intersect(new int[] {9,4,9,8,4}, new int[] {4, 9, 5});
+
+        System.out.println(result);
+    }
+}
